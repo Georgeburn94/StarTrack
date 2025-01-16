69f663b0589cb52b02b38813a627fe9d4bd4a1da
George Gaskell <georgeburn94@gmail.com>
2025-01-16 13:19:04 +0000

    Add artist and album management features with corresponding views and templates

--- startrack/urls.py
+++ startrack/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import home_page_view
+from .views import home_page_view, add_artist_view, add_album_view
 from django.contrib import admin
 
 urlpatterns = [
     path('', home_page_view, name='home'),
+    path('add-artist/', add_artist_view, name='add_artist'),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
+    path('add-album/', add_album_view, name='add_album'),
 ]
--- startrack/views.py
+++ startrack/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Artist, Album
 
 # Create your views here.
 def home_page_view(request):
+    artists = Artist.objects.all()
+    return render(request, 'home.html', {'artists': artists})
+
+def add_artist_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        bio = request.POST.get('bio')
+        Artist.objects.create(name=name, bio=bio)
+        return redirect('home')
+    return render(request, 'home.html')
+
+def add_album_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        year = request.POST.get('year')
+        artist_id = request.POST.get('artist')
+        artist = Artist.objects.get(artistID=artist_id)
+        Album.objects.create(name=name, year=year, artist=artist)
+        return redirect('home')
     return render(request, 'home.html')

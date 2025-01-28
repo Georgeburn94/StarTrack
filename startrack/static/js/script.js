9caf692b1afc1e778a8a8dba967d442b16767951
George Gaskell <georgeburn94@gmail.com>
2025-01-28 10:38:49 +0000

    feat: implement toast notifications for messages and update footer styling

--- startrack/static/js/script.js
+++ startrack/static/js/script.js
@@ -21,3 +21,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function(toastEl) {
+        return new bootstrap.Toast(toastEl)
+    })
+    toastList.forEach(toast => toast.show())
+});

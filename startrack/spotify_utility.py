6df18bd83f0c7cec3e222d8a9b362360463bb276
Georgeburn94 <georgeburn94@gmail.com>
2025-02-21 14:11:35 +0000

    prevent spotify_utility.py running on runserver. add album cover to home.html

--- startrack/spotify_utility.py
+++ startrack/spotify_utility.py
@@ -114,6 +114,14 @@ if __name__ == '__main__':
         album_id = result["id"]
         album_details = get_album_tracks_with_details(token, album_id)
         print(album_details)
+        
+    # Test parse_spotify_data_to_models
+    result = search_for_album(token, "since+i+left+you")
+    if result:
+        album_id = result["id"]
+        album_details = get_album_tracks_with_details(token, album_id)
+        parsed_result = parse_spotify_data_to_models(album_details)
+        print(parsed_result)
 
 # Parse Spotify data to models
 
@@ -127,7 +135,7 @@ def parse_spotify_data_to_models(spotify_data):
     tracks = spotify_data["tracks"]
 
     # Create or get the Artist instance
-    artist = Artist.objects.create(name=album_artist_name)
+    artist = Artist.objects.get_or_create(name=album_artist_name)
 
     # Create the Album instance and link it to the artist
     album = Album.objects.create(
@@ -147,13 +155,3 @@ def parse_spotify_data_to_models(spotify_data):
     return f"Album '{album_name}' by {album_artist_name} with {len(tracks)} tracks added successfully!"
 
 
-# Prevent execution during migrations
-if 'makemigrations' not in sys.argv and 'migrate' not in sys.argv:
-    token = get_token()
-    result = search_for_album(token, "since+i+left+you")
-
-    if result:
-        album_id = result["id"]
-        album_details = get_album_tracks_with_details(token, album_id)
-        parsed_result = parse_spotify_data_to_models(album_details)
-        print(parsed_result)
